21122b287daddcf2c918275021c401536164de0a
mahmud-21 <mmr106406@gmail.com>
2026-04-19 20:49:45 +0600

    Add main class to create and display Student objects

---
+++ Day-1(19-04-2026)/main.java
@@ -0,0 +1,22 @@
+public class main{
+    static void main(String[] args){
+        Student s1=new Student();
+        s1.name="Md Mahmudur rahman";
+        s1.Roll=10;
+        s1.district="Bhola";
+        s1.Number="01749272722";
+        s1.Show();
+        Student s2=new Student();
+        s2.name="Kutub Ali";
+        s2.Roll=11;
+        s2.district="Cumilla";
+        s2.Number="01629277953";
+        s2.Show();
+        Student s3=new Student();
+        s3.name="Hukumat Ali";
+        s3.Roll=12;
+        s3.district="chittagang";
+        s3.Number="01749123456";
+        s3.Show();
+    }
+}
