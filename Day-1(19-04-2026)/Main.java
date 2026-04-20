601d69cfa220bdec5ab3371a5118fce50d1afa38
ASUS <munnarahman9205@gmail.com>
2026-04-20 20:37:02 +0600

    The screen Shot of example 02

--- Day-1(19-04-2026)/Main.java
+++ Day-1(19-04-2026)/Main.java
@@ -1,9 +1,22 @@
-public class Main{
-    public static void main(String[] args){
-  Car myCar=new Car();
-  myCar.color="Black";
-  myCar.model="Toyota";
-  myCar.year=2025;
-  myCar.displayInfo();
+public class main{
+    static void main(String[] args){
+        Student s1=new Student();
+        s1.name="Md Mahmudur rahman";
+        s1.Roll=10;
+        s1.district="Bhola";
+        s1.Number="01749272722";
+        s1.Show();
+        Student s2=new Student();
+        s2.name="Kutub Ali";
+        s2.Roll=11;
+        s2.district="Cumilla";
+        s2.Number="01629277953";
+        s2.Show();
+        Student s3=new Student();
+        s3.name="Hukumat Ali";
+        s3.Roll=12;
+        s3.district="chittagang";
+        s3.Number="01749123456";
+        s3.Show();
     }
 }
