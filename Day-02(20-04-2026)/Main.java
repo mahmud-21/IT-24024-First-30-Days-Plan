850a1c72867c71dfe1b1c719861a19f749a77e12
mahmud-21 <mmr106406@gmail.com>
2026-04-21 23:47:19 +0600

    Create Main.java

---
+++ Day-02(20-04-2026)/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+        Student s1=new Student();
+        s1.setAge(-5);
+        s1.setAge(10);
+        System.out.println("Student's Age"+s1.getAge());
+    }
+}
