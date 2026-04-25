98e5a324311d8b7c0829e046cbf7820e0ecb3844
mahmud-21 <mmr106406@gmail.com>
2026-04-25 23:36:04 +0600

    Create Main.java

---
+++ Day-04(21-04-2026)/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+        Teacher t1=new Teacher("Dr.Ziaur Rahman","Male",1721539084);
+        t1.displayInformation();
+        Teacher t2=new Teacher("MSt. Nargis Akter","Female",1721539084);
+        t2.displayInformation();
+
+    }
+}
