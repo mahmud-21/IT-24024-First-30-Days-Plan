c163d71dbc92bdae12261b49313f0d7861f1b92f
ASUS <munnarahman9205@gmail.com>
2026-05-07 18:16:23 +0600

    The Updated code

---
+++ Day-07(25-04-2026)/Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args) {
+        Student s1=new Student("Anisul Islam",10);
+        s1.displayInfo();
+    }
+}
---
+++ Day-07(25-04-2026)/Student.java
@@ -0,0 +1,14 @@
+public class Student{
+    String name;
+    int id;
+    static String UniversityName="MBSTU";
+    Student(String n,int i){
+        name =n;
+        id=i;
+    }
+    void displayInfo(){
+        System.out.println("The name of the Student :"+name);
+        System.out.println("The nid of the Student :"+id);
+        System.out.println("The name of the university : "+UniversityName);
+    }
+}
